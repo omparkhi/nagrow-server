55e8b2b974187abf07d9fb90a1f9a6110793734e
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-20 14:13:53 +0530

    verify otp route and controller function added

--- controllers/userAuth.controller.js
+++ controllers/userAuth.controller.js
@@ -29,3 +29,34 @@ exports.sendOtp = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+exports.verifyOtp = async (req, res) => {
+  const { phone, otp } = req.body;
+
+  if (!otp || !phone) {
+    return res
+      .status(400)
+      .json({ success: false, message: "OTP or phone number is required" });
+  }
+
+  try {
+    const data = await otpModel.findOne({ phone, otp });
+
+    if (!data) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP or phone number",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "OTP verified successfully",
+      data,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Server error", error: err.message });
+  }
+};
--- routes/userAuth.route.js
+++ routes/userAuth.route.js
@@ -5,5 +5,6 @@ const userAuthController = require("../controllers/userAuth.controller");
 const router = express.Router();
 
 router.post("/send-otp", userAuthController.sendOtp);
+router.post("/verify-otp", userAuthController.verifyOtp);
 
 module.exports = router;
