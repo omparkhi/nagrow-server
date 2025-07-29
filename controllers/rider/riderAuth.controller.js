97d6e212bce360c74713780ca50f16154c5da11f
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-29 16:26:15 +0530

    created Send otp and verify otp endpoints for the rider signup process

--- controllers/rider/riderAuth.controller.js
+++ controllers/rider/riderAuth.controller.js
@@ -1,6 +1,68 @@
 const Rider = require("../../models/rider.model");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../../utils/generateToken");
+const sendOtpToPhone = require("../../services/otp.services");
+const otpModel = require("../../models/otp.model");
+
+exports.sendOtp = async (req, res) => {
+  const { phone } = req.body;
+
+  if (!phone) {
+    return res.status(400).json({ message: "Phone number is required" });
+  }
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+  try {
+    const data = await sendOtpToPhone(phone, otp);
+
+    if (data.Status === "Success") {
+      await otpModel.create({ phone, otp });
+
+      return res.json({
+        success: true,
+        message: "OTP sent successfully",
+        sessionId: data.Details,
+      });
+    } else {
+      return res.status(400).json({ success: false, message: data.Details });
+    }
+  } catch (err) {
+    console.error("Error sending OTP:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.verifyOtp = async (req, res) => {
+  const { phone, otp } = req.body;
+
+  if (!otp || !phone) {
+    return res
+      .status(400)
+      .json({ success: false, message: "OTP or phone number is required" });
+  }
+
+  try {
+    const data = await otpModel.findOne({ phone, otp });
+
+    if (!data) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP or phone number",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "OTP verified successfully",
+      data,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Server error", error: err.message });
+  }
+};
 
 exports.registerRider = async (req, res) => {
   const { name, phone, email, password, location } = req.body;
--- routes/rider/riderAuth.route.js
+++ routes/rider/riderAuth.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const riderAuthController = require("../../controllers/rider/riderAuth.controller");
 
+router.post("/send-otp", riderAuthController.sendOtp);
+router.post("/verify-otp", riderAuthController.verifyOtp);
 router.post("/register", riderAuthController.registerRider);
 router.post("/login", riderAuthController.loginRider);
 
